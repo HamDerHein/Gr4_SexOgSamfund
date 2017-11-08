81bfc108a7d2f8e057840800e679e704af779e6d
Mikkel Hein <Hein@desktop-77hir8b.kea.local>
2017-11-08 14:05:26 +0100

    added functions

---
+++ javascript.js
@@ -0,0 +1,247 @@
+function skolegaarden() {
+
+
+
+}
+
+function valgAfKarakter() {
+
+
+
+}
+
+function sofiaKommerInd() {
+
+
+
+}
+
+function sofiaBloeder() {
+
+
+
+}
+
+function mobilInd() {
+
+
+
+}
+
+function tagerBilledet() {
+
+
+
+}
+
+function mobilUd() {
+
+
+
+}
+
+function karinaOgVeninderKommerInd() {
+
+
+
+}
+
+function karinaOgVeninderFniser() {
+
+
+
+}
+
+function lydStopper() {
+
+
+
+}
+
+// mobilInd funktion skal genbruges her.
+
+
+function SofiaHarFaaetNotifikation() {
+
+
+
+}
+
+function graeder() {
+
+
+
+}
+
+function zoomMobil() {
+
+
+
+}
+
+function karinaValget() {
+
+
+
+}
+
+// Her skal mobilUd genbruges
+
+function karinaFaarNotifikation() {
+
+
+
+}
+
+function karinaDaarligSamvittighed() {
+
+
+
+}
+
+function karinaHvadSKalJegGore() {
+
+
+
+}
+
+function karinaValget() {
+
+
+
+}
+
+// KARINAS VALG !!!!!!
+
+// KARINAS FØRSTE VALG (A)
+
+
+function karinaKontakteLaerer() {
+
+
+
+}
+
+function laererErKommetInd() {
+
+
+
+}
+
+function KarinaSporgerLaerer() {
+
+
+
+}
+
+function laererSvarerKarina() {
+
+
+
+}
+
+
+// KARINAS ANDET VALG (B)
+
+
+// HER SKAL mobilInd genbruges igen
+
+function privatSnakPaaMobil() {
+
+
+
+}
+
+function karinaHenTilSofia() {
+
+
+
+}
+
+function sofiaGraeder() {
+
+
+
+}
+
+function karinaSporgerSofia() {
+
+
+
+}
+
+function sofiaDetVedJegIkke() {
+
+
+
+}
+
+function sofiaKontaktPrivatSnak() {
+
+
+
+}
+
+//
+// GENTAGELSE AF HELE FØRSTE DEL - FRA "sofiaKommerInd" TIL "sofiaGraeder"
+//
+
+function sofiaValget() {
+
+
+
+}
+
+function sofiaKontakteLaerer() {
+
+
+
+}
+
+function laererErKommetIndTilSofia() {
+
+
+
+}
+
+function sofiaSporgerLaerer() {
+
+
+
+}
+
+function laererSvarerSofia() {
+
+
+
+}
+
+function sofiaMobilInd() {
+
+
+
+}
+
+function bogstavKommerInd() {
+
+
+
+}
+
+function knapKommerFrem() {
+
+
+
+}
+
+function trykPaaKnap() {
+
+
+
+}
+
+function brugerenTrykker() {
+
+
+
+}
